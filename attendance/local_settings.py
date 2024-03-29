c22762357b042b230a6de5c969a03395c6df890f
leo88 <nishinole88@gmail.com>
2024-03-29 23:19:08 +0900

    first commit

---
+++ attendance/local_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "attendanceDB",
+        "USER": "root",
+        "PASSWORD": "leo08178",
+        "HOST": "localhost",
+        "PORT": "3306",
+    }
+}
