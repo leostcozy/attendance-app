3b5a1862abf159aefa9e5035b1e45fb0b95cd48d
leo88 <nishinole88@gmail.com>
2024-04-07 18:01:30 +0900

    微調整(デプロイ前)

--- attendance/models.py
+++ attendance/models.py
@@ -10,5 +10,6 @@ class Attendances(models.Model):
     break_start_time = models.DateTimeField(null=True, blank=True)  # 休憩開始時間
     break_end_time = models.DateTimeField(null=True, blank=True) 
     
+    
     def __str__(self):
         return self.user.username
